4f58d49ee65864025360940175179ae84d746cf0
Quocanh Kieu <kieuquocanh4@gmail.com>
2023-07-17 01:22:51 +0700

    toFixed

--- js/style.js
+++ js/style.js
@@ -1,3 +1,12 @@
-// $(document) {
-
-// } 
+$(function () {
+  $(window).on("scroll", function (e) {
+    console.log(window);
+    console.log(window.scrollY);
+    if (window.scrollY > 300) {
+      console.log("line 6");
+      $(".containerParent.forHeader").addClass("toFixed");
+    } else {
+      $(".containerParent.forHeader").removeClass("toFixed");
+    }
+  });
+});
